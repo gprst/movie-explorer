96029991d925ca36792670c605980613d5721184
gprst <32643653+gprst@users.noreply.github.com>
2022-03-20 14:37:55 +0100

    feat(router): set up routes and basic pages components

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App'
+import MovieDetailsPage from './pages/MovieDetails/MovieDetailsPage';
+import MoviesPage from './pages/Movies/MoviesPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="" element={<MoviesPage />} />
+          <Route path="/movie/:id" element={<MovieDetailsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
