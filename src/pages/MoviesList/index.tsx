4aa75116a809b1e86da3803a9a514eb5f9cdc717
gprst <32643653+gprst@users.noreply.github.com>
2022-03-27 19:39:04 +0200

    feat(movies-list): redirect to homepage if there is no search

--- src/pages/MoviesList/index.tsx
+++ src/pages/MoviesList/index.tsx
@@ -1,10 +1,19 @@
 import { Container, Stack } from '@mui/material';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SearchContext from '~/contexts/SearchContext';
 import MovieCard from './MovieCard';
 
 export default function Movies() {
   const { searchResult } = useContext(SearchContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (Object.entries(searchResult).length === 0) {
+      navigate('/');
+    }
+  }, []);
 
   return (
     <Container maxWidth="lg" sx={{ my: 6, display: 'flex', justifyContent: 'center' }}>
